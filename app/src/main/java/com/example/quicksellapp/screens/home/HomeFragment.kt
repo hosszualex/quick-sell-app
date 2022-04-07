b6d6229aa12f7ec12b3f02264d5f188ea35f1149
Alex Hosszu <hosszualex99@gmai.com>
2022-04-07 11:43:49 +0300

    Working language changer
    - working language changer

--- app/src/main/java/com/example/quicksellapp/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/quicksellapp/screens/home/HomeFragment.kt
@@ -1,7 +1,10 @@
 package com.example.quicksellapp.screens.home
 
+import android.content.Context
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
@@ -9,6 +12,7 @@ import com.example.quicksellapp.Constants
 import com.example.quicksellapp.R
 import com.example.quicksellapp.databinding.FragmentHomeBinding
 import com.example.quicksellapp.extensions.addFragmentOnTopWithAnimationLeftToRight
+import com.example.quicksellapp.extensions.put
 import com.example.quicksellapp.screens.quicksell.QuickSellFragment
 
 class HomeFragment: Fragment() {
@@ -22,7 +26,6 @@ class HomeFragment: Fragment() {
         //TODO ask for token image
         return initializeScreen(inflater)
     }
-
     private fun initializeScreen(inflater: LayoutInflater): View {
         binding = FragmentHomeBinding.inflate(inflater)
         binding.lifecycleOwner = this
@@ -33,6 +36,17 @@ class HomeFragment: Fragment() {
         (binding.tilLanguageMenu.editText as? AutoCompleteTextView)?.setAdapter(adapter)
         binding.btnQuickSell.setOnClickListener {
             activity?.addFragmentOnTopWithAnimationLeftToRight(QuickSellFragment(), Constants.QUICK_SELL_SCREEN_TAG)
+        }
+        binding.tvLanguage.setOnItemClickListener { _, _, _, _ ->
+            val languageText = binding.tvLanguage.text.toString()
+            val sharedPreferences =
+                requireContext().getSharedPreferences("name", Context.MODE_PRIVATE)
+            if (languageText == "English") {
+                sharedPreferences.put("LANG", "en")
+            } else {
+                sharedPreferences.put("LANG", "ro")
+            }
+            requireActivity().recreate()
         }
         return binding.root
     }
